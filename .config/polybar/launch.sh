cc6cd8fb6fbed37117c860a2c99be27668035bba
Chester <nestor_chavez@hotmail.es>
2021-11-05 22:06:56 -0600

    Se creo tema de rofi Nord, se modifico tema de polybar con Nord

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,9 +11,11 @@ launch_bar() {
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 	# Launch the bar
-	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
-		polybar -q top -c "$dir/$style/config.ini" &
-		polybar -q bottom -c "$dir/$style/config.ini" &
+	if [[ "$style" == "nord" || "$style" == "cuts" ]]; then
+		#polybar -q top -c "$dir/$style/config.ini" &
+		#polybar -q bottom -c "$dir/$style/config.ini" &
+      polybar -q nord-top -c $dir/$style/config.ini &
+      polybar -q nord-down -c $dir/$style/config.ini &
 	elif [[ "$style" == "pwidgets" ]]; then
 		bash "$dir"/pwidgets/launch.sh --main
 	else
@@ -33,40 +35,8 @@ elif [[ "$1" == "--default" ]]; then
 	style="default"
 	launch_bar
 
-elif [[ "$1" == "--hack" ]]; then
-	style="hack"
-	launch_bar
-
-elif [[ "$1" == "--docky" ]]; then
-	style="docky"
-	launch_bar
-
-elif [[ "$1" == "--cuts" ]]; then
-	style="cuts"
-	launch_bar
-
-elif [[ "$1" == "--shapes" ]]; then
-	style="shapes"
-	launch_bar
-
-elif [[ "$1" == "--grayblocks" ]]; then
-	style="grayblocks"
-	launch_bar
-
-elif [[ "$1" == "--blocks" ]]; then
-	style="blocks"
-	launch_bar
-
-elif [[ "$1" == "--colorblocks" ]]; then
-	style="colorblocks"
-	launch_bar
-
-elif [[ "$1" == "--forest" ]]; then
-	style="forest"
-	launch_bar
-
-elif [[ "$1" == "--pwidgets" ]]; then
-	style="pwidgets"
+elif [[ "$1" == "--nord" ]]; then
+	style="nord"
 	launch_bar
 
 elif [[ "$1" == "--panels" ]]; then
