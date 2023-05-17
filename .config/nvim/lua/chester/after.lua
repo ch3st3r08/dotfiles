64e19b9abe5427dc5c1e2b26317b0a1457cdc4cc
Chester <nestor_chavez@hotmail.es>
2023-05-17 17:52:48 -0600

    Adding some tmux config

--- .config/nvim/lua/chester/after.lua
+++ .config/nvim/lua/chester/after.lua
@@ -1,3 +1,6 @@
+-- Colorscheme
+vim.cmd.colorscheme('tokyonight')
+--
 -- Transparent plugin
 vim.g.transparent_groups = vim.list_extend(
   vim.g.transparent_groups or {},
@@ -6,24 +9,17 @@ vim.g.transparent_groups = vim.list_extend(
   end, vim.tbl_values(require("bufferline.config").highlights))
 )
 
--- Startify plugin
-vim.g.startify_lists = {
-  { header = { '  Bookmarks' },                             type = 'bookmarks' },
-  { header = { '  Sessions' },                              type = 'sessions' },
-  { header = { '  Files' },                                 type = 'files' },
-  { header = { '  Current Directory ' .. vim.fn.getcwd() }, type = 'dir' },
-}
-
-vim.g.startify_bookmarks = {
-  { b = '~/.config/zsh/.zshrc' },
-  { v = '~/.config/nvim/init.lua' }
-}
-
-vim.g.startify_fortune_use_unicode = 1
 
 -- Algunos comandos
 vim.api.nvim_create_user_command('SSave',
   function(opts)
     MiniSessions.write(opts.fargs[1])
+  end, { nargs = '?' }
+)
+
+-- Cerrar todos los buffers menos el actual
+vim.api.nvim_create_user_command('Bc',
+  function()
+    vim.cmd('%bd|e#|bd#')
   end, { nargs = '?' }
 )
--- .config/nvim/lua/chester/plugins/lualine.lua
+++ .config/nvim/lua/chester/plugins/lualine.lua
@@ -2,12 +2,12 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     config = function()
-      local custom_auto = require 'lualine.themes.auto'
+      local custom_auto = require 'lualine.themes.tokyonight'
       custom_auto.normal.c.bg = 'NONE'
 
       require 'lualine'.setup({
         options = {
-          theme = "auto",
+          theme = custom_auto,
           icons_enabled = true,
           disabled_filetypes = {
             winbar = { "NvimTree", "startify" },
