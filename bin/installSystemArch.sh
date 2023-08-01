933fa21c2c114cb2395eb2cb2e5bc0311e458894
Chester <nestor_chavez@hotmail.es>
2023-07-31 19:47:21 -0600

    Refactoring Arch Install script

--- bin/installSystemArch.sh
+++ bin/installSystemArch.sh
@@ -2,47 +2,7 @@
 
 _DEBUG=0
 IS_VM=0
-
-while getopts 'hvd' OPTION; do
-  case "$OPTION" in
-    h)
-      echo "Usage: $0 [-h] [-v]"
-      ;;
-    v)
-      IS_VM=1
-      ;;
-    d)
-      _DEBUG=1
-      ;;
-    *)
-      echo "Usage: $0 [-h] [-v]" >&2
-      exit 1
-      ;;
-  esac
-done
-
-shift "$(($OPTIND -1))"
-
-error() {
-  printf '\E[31m'; echo "$@"; printf '\E[0m'
-}
-
-info() {
-  printf '\E[32m'; echo "$@"; printf '\E[0m'
-}
-
-warn(){
-  printf '\E[33m'; echo "$@"; printf '\E[0m'
-}
-
-ask() {
-  local continuar
-  warn "Desea continuar con el siguiente paso (y/n)?: "
-  read continuar
-  if [[ ! $continuar = "y" ]]; then
-    exit 1
-  fi
-}
+WM="xfce4"
 
 #Descargar e instalar Paru (AUR helper)
 git clone https://aur.archlinux.org/paru.git Sources/paru
@@ -52,9 +12,18 @@ makepkg -si
 cd $HOME
 
 echo "---------------------------------------------------------------"
-info "Instalando archivos necesarios"
-paru -S base-devel curl libsecret pacman-contrib xorg i3 polybar picom network-manager-applet zsh tmux gvfs gvfs-smb thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman xarchiver rofi pulseaudio pulseaudio-alsa polkit-gnome xfce4-power-manager lightdm lightdm-gtk-greeter-settings light-locker light-locker-settings dunst dex desktop-file-utils xdg-utils xdg-user-dirs htop neofetch neovim starship alacritty community/exa community/ripgrep bat fd unzip seahorse mugshot grub-customizer ffmpeg mpv pavucontrol brave-browser gtk-engine-murrine lxappearance mpd ncmpcpp flameshot nitrogen xclip xpad font-manager seahorse feh zathura zathura-pdf-mupdf ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd plymouth-theme-arch10
+echo "Instalando archivos necesarios"
+paru -S base-devel curl libsecret pacman-contrib xorg network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber lightdm lightdm-gtk-greeter-settings light-locker light-locker-settings xdg-utils xdg-user-dirs neofetch neovim starship alacritty unzip mpv pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd plymouth plymouth-theme-arch10
 
+
+if [ $WM == "i3" ]; then
+	paru -S i3 polybar picom polkit-gnome dex xfce4-power-manager rofi dunst lxappearance nitrogen thunar flameshot desktop-file-utils libnotify xpad
+else
+	paru -S xfce4 xfce4-goodies 
+fi
+
+# Instalando aplicacion de utilidad
+paru -S community/exa community/ripgrep bat fd xclip zathura zathura-pdf-mupdf htop seahorse mugshot grub-customizer ffmpeg font-manager mpv vlc sct tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman 
 
 echo "---------------------------------------------------------------"
 info "Instalando #-NVM (node version manager)"
@@ -78,33 +47,34 @@ sudo cp -R Sources/NordArc_icon_theme/NordArc-Icons /usr/share/icons
 sudo cp -R Sources/Nordzy-cursors/Nordzy-cursors /usr/share/icons
 sudo cp -R Sources/Nordzy-cursors/Nordzy-cursors-white /usr/share/icons
 
-info "Configurando propiedades default del lightdm-gtk-greeter-settings"
-sudo tee -a <<'EOF' /etc/lightdm/lightdm-gtk-greeter.conf >/dev/null
+if [ $WN == "i3" ]; then
+
+sudo tee /etc/lightdm/lightdm-gtk-greeter.conf > /dev/null <<'EOF'
 [greeter]
 background = /usr/share/backgrounds/display-manager-bg/background
 theme-name = Nordic
 icon-theme-name = NordArc-icons
 EOF
+	# "Creando carpeta display-manager-bg en /usr/share/backgrounds"
+	sudo mkdir -p /usr/share/backgrounds/display-manager-bg
+	sudo cp -v Sources/background/default_wallpaper.png /usr/share/backgrounds/display-manager-bg/background
 
-info "Creando carpeta display-manager-bg en /usr/share/backgrounds"
-sudo install -d /usr/share/backgrounds/display-manager-bg
-sudo cp -v Sources/background/default_wallpaper.png /usr/share/backgrounds/display-manager-bg/background
+	# "Creando archivo bg-saved.cfg"
+	NITRO_PATH=$HOME_USUARIO/.config/nitrogen
 
-info "Creando archivo bg-saved.cfg"
-NITRO_PATH=$HOME/.config/nitrogen
-
-mkdir -p $NITRO_PATH
-cat <<EOF >> $NITRO_PATH/bg-saved.cfg
+	mkdir -p $NITRO_PATH
+	cat <<EOF >> $NITRO_PATH/bg-saved.cfg
 [xin_-1]
 file=$HOME/Sources/background/default_wallpaper.png
 mode=0
 bgcolor=#000000
 EOF
+fi
 
-info "Descargando tema de grub en Downloads"
+# "Descargando tema de grub en Downloads"
 curl -Lo Downloads/arch.tar https://github.com/AdisonCavani/distro-grub-themes/releases/latest/download/arch.tar
 
-info "Iniciando servicios de lightdm"
+# "Iniciando servicios de lightdm"
 sudo systemctl enable --now lightdm
 
-info "La instalacion del sistema ha terminado, se recomienda REINICIAR el sistema, para que los cambios surjan efecto."
+echo "La instalacion del sistema ha terminado, se recomienda REINICIAR el sistema, para que los cambios surjan efecto."
