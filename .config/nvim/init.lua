67148f34462b6647883483d2fce1bbf6c02dd4a9
Chester <nestor_chavez@hotmail.es>
2023-12-16 16:37:59 -0600

    remove old nvim config and using lazyvim, fixing libsecret path in gitconfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,2 @@
-require("chester")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- .config/nvim/lua/chester/init.lua
+++
@@ -1,4 +0,0 @@
-require("chester.options")
-require("chester.mappings")
-require("chester.lazy")
-require("chester.after")
