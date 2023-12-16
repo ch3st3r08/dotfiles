eeda0ffc77970dd3d28fd843a4d57cb9f08d3508
Chester <nestor_chavez@hotmail.es>
2023-12-16 16:40:20 -0600

    Adding lazyvim config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+local opts = { noremap = true, silent = true }
+
+-- vim.keymap.del("n", "<C-h>")
+-- vim.keymap.del("n", "<C-j>")
+-- vim.keymap.del("n", "<C-k>")
+-- vim.keymap.del("n", "<C-l>")
+
+vim.keymap.set("n", "J", "mzJ`z", opts)
+
+-- C-d y C-u mejorados para mantener el cursor en el mismo lugar
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
