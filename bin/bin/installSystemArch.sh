496809150bd0a4ab626e3e4f5bf8c8064b7d6ab9
Chester <nestor_chavez@hotmail.es>
2024-02-09 23:07:48 -0600

    Modify readme, new installations instructions

--- bin/bin/installSystemArch.sh
+++ bin/bin/installSystemArch.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 _DEBUG=0
-IS_VM=0
-WM="xfce4"
+IS_VM=1
+WM="hypr" #xfce4
+
+pre_packages=""
+if [ -z $(which stows 2>/dev/null) ]; then
+	pre_packages="${pre_packages} stow"
+fi
+
+if [ -z $(which xdg-user-dirs-updates 2>/dev/null) ]; then
+	pre_packages="${pre_packages} xdg-user-dirs"
+fi
+
+if ! [ -z "$pre_packages" ]; then
+	sudo pacman -S $pre_packages
+fi
+
+cd $HOME
+
+#XDG directories
+xdg-user-dirs-update
 
 #Descargar e instalar Paru (AUR helper)
 git clone https://aur.archlinux.org/paru.git Sources/paru
@@ -11,68 +29,32 @@ makepkg -si
 
 cd $HOME
 
+rm -f $HOME/.bashrc
+
+git clone --recurse-submodules https://github.com/ch3st3r08/dotfiles .dotfiles
+cd .dotfiles
+stow -S alacritty hypr nvim starship git waybar tmux zsh rofi sources music bin bash misc
+
 echo "---------------------------------------------------------------"
 echo "Instalando archivos necesarios"
-paru -S base-devel curl libsecret pacman-contrib xorg network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber lightdm lightdm-gtk-greeter-settings light-locker light-locker-settings xdg-utils xdg-user-dirs neofetch neovim starship alacritty unzip mpv pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd plymouth plymouth-theme-arch10
+paru -S curl pacman-contrib networkmanager network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber thunar lightdm lightdm-gtk-greeter-settings xdg-utils fastfetch neovim starship alacritty unzip mpv pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp rhythmbox
 
-
-if [ $WM == "i3" ]; then
-	paru -S i3 polybar picom polkit-gnome dex xfce4-power-manager rofi dunst lxappearance nitrogen thunar flameshot desktop-file-utils libnotify xpad
+if [ $WM == "hypr" ]; then
+	paru -S waybar rofi swaync swayidle waylock nwg-look-bin azote polkit-kde-agent desktop-file-utils nwg-displays nwg-drawer-bin nwg-shell-config nwg-icon-picker nwg-menu-bin hyprpicker gtklock wf-recorder ristretto cliphist galculator
 else
-	paru -S xfce4 xfce4-goodies 
+	paru -S xfce4 xfce4-goodies light-locker light-locker-settings mugshot sctwmctrl timeshift
 fi
 
 # Instalando aplicacion de utilidad
-paru -S community/exa community/ripgrep bat fd xclip zathura zathura-pdf-mupdf htop seahorse mugshot grub-customizer ffmpeg font-manager mpv vlc sct tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman 
+paru -S community/eza community/ripgrep bat fd fzf lazygit xclip zathura zathura-pdf-mupdf htop seahorse grub-customizer ffmpeg font-manager mpv vlc sct reflector tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd ttf-font-awesome otf-codenewroman-nerd plymouth plymouth-theme-arch10 dracula-gtk-theme nordic-theme nordzy-cursors nordzy-icon-theme whitesur-icon-theme
 
 echo "---------------------------------------------------------------"
 info "Instalando #-NVM (node version manager)"
 export NVM_DIR="$HOME/.nvm"
 git clone https://github.com/nvm-sh/nvm.git $NVM_DIR
 cd $NVM_DIR
-git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
 cd $HOME
-
-
-info "Creando directorio de themes e íconos"
-mkdir -p $HOME/.local/share/themes
-mkdir -p $HOME/.local/share/icons
-
-info "Instalando tema Nord, Nord Icon y Nord Cursor de forma Global"
-git clone https://github.com/EliverLara/Nordic Sources/Nordic_theme
-git clone https://github.com/robertovernina/NordArc Sources/NordArc_icon_theme
-git clone https://github.com/alvatip/Nordzy-cursors Sources/Nordzy-cursors
-sudo cp -R Sources/Nordic_theme /usr/share/themes/Nordic
-sudo cp -R Sources/NordArc_icon_theme/NordArc-Icons /usr/share/icons
-sudo cp -R Sources/Nordzy-cursors/Nordzy-cursors /usr/share/icons
-sudo cp -R Sources/Nordzy-cursors/Nordzy-cursors-white /usr/share/icons
-
-if [ $WN == "i3" ]; then
-
-sudo tee /etc/lightdm/lightdm-gtk-greeter.conf > /dev/null <<'EOF'
-[greeter]
-background = /usr/share/backgrounds/display-manager-bg/background
-theme-name = Nordic
-icon-theme-name = NordArc-icons
-EOF
-	# "Creando carpeta display-manager-bg en /usr/share/backgrounds"
-	sudo mkdir -p /usr/share/backgrounds/display-manager-bg
-	sudo cp -v Sources/background/default_wallpaper.png /usr/share/backgrounds/display-manager-bg/background
-
-	# "Creando archivo bg-saved.cfg"
-	NITRO_PATH=$HOME_USUARIO/.config/nitrogen
-
-	mkdir -p $NITRO_PATH
-	cat <<EOF >> $NITRO_PATH/bg-saved.cfg
-[xin_-1]
-file=$HOME/Sources/background/default_wallpaper.png
-mode=0
-bgcolor=#000000
-EOF
-fi
-
-# "Descargando tema de grub en Downloads"
-curl -Lo Downloads/arch.tar https://github.com/AdisonCavani/distro-grub-themes/releases/latest/download/arch.tar
 
 # "Iniciando servicios de lightdm"
 sudo systemctl enable --now lightdm
--- readme.md
+++ readme.md
@@ -2,127 +2,70 @@
 
 ## Lo primero: Instalar Sudo y Git, agregarnos al grupo sudo y generar los directorios XDG, y cerrar sesion
 
-### Debian:
-      su -
-      apt install sudo git -y
-      usermod -aG sudo [user]
-      exit
-      xdg-user-dirs-update
-      exit
-      
 ### Arch Linux:
-      su -
-      pacman -Syu sudo git --noconfirm
+      pacman -Syu sudo git xdg-user-dirs --noconfirm
       usermod -aG sudo [user]
-      exit
       xdg-user-dirs-update
       exit
 
 ## Luego hay que descargar los DOTFILES de github
-      mkdir -p .dotfiles
-      alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-      echo ".dotfiles" >> .gitignore
-      git clone --bare https://github.com/ch3st3r08/dotfiles $HOME/.dotfiles
+      git clone https://github.com/ch3st3r08/dotfiles $HOME/.dotfiles
       rm -f .bashrc
-      config checkout
-      config config --local status.showUntrackedFiles no
 
 ## Luego ejecutar el script de instalación que se encuentra en bin/installSystem.sh
 
 ## Luego de la instalacion...
-Antes de reiniciar es necesario modificar 
-**Si se usa máquina virtual** :
-- Comentar linea **v-sync** en la configuración de Picom
-- Cambiar de GLX a XRENDER en picom.conf
-
-**Si se utiliza un WM (i3, sway)**
-- Cambiar nombre de interfaz de red en la configuración de Polybar
-- Eliminar la configuración de red en **/etc/network/interfaces**
-- Modificar el **Hidden=true** en el desktop file de lxpolkit en **/etc/xdg/autostart**
-- Activar los temas con lxappearance (tema, iconos, cursores)
-
 Para tema de grub
 - Modificar el tema de grub con grub customizer
   - agregar linea **GRUB_BACKGROUND** al archivo **/etc/default/grub**
   - modificar la linea **GRUB_GFXMODE** y agregar resolucion **1090x1200x24,auto**
   - agregar la linea **GRUB_DISABLE_OS_PROBER=false** al archivo **/etc/default/grub**
   
-Extras:
-- Instalar los LSP servers, luego de abrir NVIM
-
 # Listado de paquetes principales instalados
-- Build-essential
-- Include backports
-- Libsecret
 - Git
 - Curl
-- DOTFILES
-- XORG (Desktop Compositor)
-- I3wm (Window Manager)
-- Polybar (StatusBar)
-- Picom (Compositor)
+- Hyperland (Wayland Compositor)
+- Waybar (StatusBar)
+- Alacritty (Terminal Emulator)
+- Neovim (Text editor)
 - Network Manager (Network Control)
-- PulseAudio (Audio server, frontend for ALSA) (Audio Control)
+- Pipewire (Audio server, frontend for ALSA) (Audio Control)
 - Polkit Agent (Polkit Auth Agent)
-- Xfce4-power-manager (Power Manager)
 - LightDM (Display Manager)
-- Light-locker (Screen Locker)
+- Swaylock (Screen Locker)
 - Rofi (Application Launcher)
-- Dunst (Notification service)
-- Nitrogen (Background setter)
+- Swaync (Notification service)
+- Azote (Background setter)
+
+# Utils
+- Starship (Prompt Customizer)
+- Xpad (Note app)
+- Lxterminal (Alternative terminal)
+- Pavucontrol (Audio Control)
+- Fastfetch (System stats)
+- Nerdfonts (Icon fonts)
+- Nwg-look (Look and Feel)
+- Tty-clock (terminal clock)
+- Bdsmainutils (terminal calendar)
+- Cava (Console Audio Visualizer)
 
 # Default Applications
-- Dex (XDG-AUTOSTART implementation)
-- XDG utils
-- NVM (node package manager)
-- Neofetch
-- Neovim
-- Rustup
-- Startship
-- Alacritty
-- Lxterminal
-- Nerdfonts
-- Lxappearance (GTK theme customizer)
-- Pcmanfm (GUI File Manager)
-- Ranger (Console file manager)
-- Feh (Simple Image Viewer)
-- Pavucontrol (PulseAudio Control)
-- MuPDF (Lightweight PDF Reader)
-- Mugshot (Account Services)
-- Grub Customizer
-- Bdsmainutils (terminal calendar)
-- Tty-clock (terminal clock)
-- Cava (Console Audio Visualizer)
+- Thunar (GUI File Manager)
+- Brave (Browser)
+- Mpv, VLC (Media Player)
+- Zathura (pdf reader)
+- Rythmbox (Audio Player)
+- Ristretto (Image viewer)
+- Zathura, Evince (PDF Reader)
+- Font Manager
+- Galculator
+- Seahorse (gpnu keys and passwords)
+
 
 ### At this point, reboot the system
 
 ## Listado de aplicaciones GUI instaladas
-- VLC
-- Font-manager
-- Evince (advanced PDF Reader)
-- galculator (calculadora científica)
-- Seahorse (gpnu keys and passwords)
 - OpenShoot (Video editor)
 - OBS (Screen Recorder)
 - youtube-dl
-- Pureline
 - ffmpeg
-- xpad
-- sct (Temperature monitor)
-- mpv (Video Player)
-
-### Firmware 
-- firmware-realtek
-- firmware-misc-nonfree
-
-### Temas (Crear directorio .themes .icons .local/share/themes .local/share/icons)
-- Nord Theme (crear directorio .themes) https://github.com/EliverLara/Nordic
-- NordArc Icon theme (crear directorio .icons) https://github.com/robertovernina/NordArc
-
-### Fonts (URL: https://github.com/ryanoasis/nerd-fonts)
-- Sauce Code Pro Nerd Font
-- TerminessTTF Nerd Font
-- Fira Regular Nerd Font
-- Hack Regular Nerd Font
-- Anonymice Nerd Font
-- DejaVu Sans Mono Nerd Font
