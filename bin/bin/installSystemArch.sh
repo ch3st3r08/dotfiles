cf6ef976d77c95b94cb56415908066329f40c29c
Chester <nestor_chavez@hotmail.es>
2024-02-10 20:11:25 -0600

    Using sddm instead

--- bin/bin/installSystemArch.sh
+++ bin/bin/installSystemArch.sh
@@ -37,16 +37,16 @@ stow -S alacritty hypr nvim starship git waybar tmux zsh rofi sources music bin 
 
 echo "---------------------------------------------------------------"
 echo "Instalando archivos necesarios"
-paru -S curl pacman-contrib networkmanager network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber thunar lightdm lightdm-gtk-greeter-settings xdg-utils fastfetch neovim starship alacritty unzip mpv pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp rhythmbox
+paru -S curl pacman-contrib networkmanager network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber gst-plugin-pipewire thunar xdg-utils fastfetch neovim starship alacritty unzip pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp rhythmbox xorg-server xorg-xinit iwd libva-mesa-driver intel-media-driver sound-theme-freedesktop xf86-video-amdgpu
 
 if [ $WM == "hypr" ]; then
-	paru -S waybar rofi swaync swayidle waylock swaybg nwg-look-bin azote polkit-kde-agent desktop-file-utils nwg-displays nwg-drawer-bin nwg-shell-config nwg-icon-picker nwg-menu-bin hyprpicker gtklock wf-recorder ristretto cliphist galculator
+	paru -S hyprland xdg-desktop-portal-hyprland sddm waybar rofi swaync swayidle waylock swaybg nwg-look-bin azote polkit polkit-kde-agent desktop-file-utils nwg-displays nwg-drawer-bin nwg-shell-config nwg-icon-picker nwg-menu-bin hyprpicker gtklock wf-recorder ristretto cliphist galculator qt5-wayland qt6-wayland
 else
-	paru -S xfce4 xfce4-goodies light-locker light-locker-settings mugshot sct wmctrl timeshift
+	paru -S xfce4 xfce4-goodies mugshot sct wmctrl timeshift xclip
 fi
 
 # Instalando aplicacion de utilidad
-paru -S eza ripgrep bat fd fzf lazygit xclip zathura zathura-pdf-mupdf htop seahorse grub-customizer ffmpeg font-manager mpv vlc sct reflector tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd ttf-font-awesome otf-codenewroman-nerd plymouth plymouth-theme-arch10 dracula-gtk-theme nordic-theme nordzy-cursors nordzy-icon-theme whitesur-icon-theme
+paru -S eza ripgrep bat fd fzf lazygit zathura zathura-pdf-mupdf htop seahorse grub-customizer ffmpeg font-manager mpv vlc reflector tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd ttf-font-awesome otf-codenewroman-nerd plymouth plymouth-theme-arch10 dracula-gtk-theme nordic-theme nordzy-cursors nordzy-icon-theme whitesur-icon-theme
 
 echo "---------------------------------------------------------------"
 info "Instalando #-NVM (node version manager)"
@@ -57,6 +57,6 @@ git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list -
 cd $HOME
 
 # "Iniciando servicios de lightdm"
-sudo systemctl enable --now lightdm
+sudo systemctl enable --now sddm.service
 
 echo "La instalacion del sistema ha terminado, se recomienda REINICIAR el sistema, para que los cambios surjan efecto."
