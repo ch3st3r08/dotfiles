8f83957e8dc57dff743cbb7cf391c7166ce6afa1
Chester <nestor_chavez@hotmail.es>
2024-03-22 17:32:55 -0600

    Fixing typos and new aliases
    Fixing install script typos, adding new aliases on zsh

--- bin/bin/installSystemArch.sh
+++ bin/bin/installSystemArch.sh
@@ -14,7 +14,7 @@ if [ -z $(which xdg-user-dirs-update 2>/dev/null) ]; then
 fi
 
 if ! [ -z "$pre_packages" ]; then
-	sudo pacman -S $pre_packages
+	sudo pacman -S --needed --noconfirm $pre_packages
 fi
 
 cd $HOME
@@ -23,7 +23,7 @@ cd $HOME
 xdg-user-dirs-update
 
 #Descargar e instalar Paru (AUR helper)
-git clone https://aur.archlinux.org/paru.git Sources/paru-bin
+git clone https://aur.archlinux.org/paru-bin.git Sources/paru
 cd Sources/paru
 makepkg -si
 
@@ -36,19 +36,19 @@ stow -S alacritty hypr nvim starship git waybar tmux zsh rofi sources music bin 
 
 echo "---------------------------------------------------------------"
 echo "Instalando archivos necesarios"
-paru -S curl pacman-contrib networkmanager sddm network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber gst-plugin-pipewire thunar xdg-utils fastfetch neovim starship alacritty unzip pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp rhythmbox xorg-server xorg-xinit iwd libva-mesa-driver intel-media-driver sound-theme-freedesktop xf86-video-amdgpu galculator
+paru -S --needed --noconfirm curl pacman-contrib networkmanager sddm network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber gst-plugin-pipewire thunar xdg-utils fastfetch neovim starship alacritty unzip pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp rhythmbox xorg-server xorg-xinit iwd libva-mesa-driver intel-media-driver sound-theme-freedesktop xf86-video-amdgpu galculator
 
 if [ $WM == "hypr" ]; then
-	paru -S hyprland xdg-desktop-portal-hyprland waybar rofi swaync hypridle hyprlock swaybg nwg-bar nwg-look-bin waypaper polkit polkit-kde-agent desktop-file-utils nwg-displays nwg-drawer-bin nwg-icon-picker nwg-menu-bin hyprpicker wf-recorder ristretto cliphist qt5-wayland qt6-wayland
+	paru -S --needed --noconfirm hyprland xdg-desktop-portal-hyprland waybar rofi swaync hypridle hyprlock swaybg nwg-bar nwg-look-bin waypaper polkit polkit-kde-agent desktop-file-utils nwg-displays nwg-drawer-bin nwg-icon-picker nwg-menu-bin hyprpicker wf-recorder ristretto cliphist qt5-wayland qt6-wayland
 else
-	paru -S xfce4 xfce4-goodies mugshot sct wmctrl timeshift xclip
+	paru -S --needed --noconfirm xfce4 xfce4-goodies mugshot sct wmctrl timeshift xclip
 fi
 
 # Instalando aplicacion de utilidad
-paru -S p7zip entr zoxide eza ripgrep bat bat-extras fd fzf lazygit zathura zathura-pdf-mupdf htop seahorse grub-customizer ffmpeg font-manager mpv vlc reflector tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd ttf-font-awesome otf-codenewroman-nerd plymouth plymouth-theme-arch10 dracula-gtk-theme nordic-theme nordzy-cursors nordzy-icon-theme whitesur-icon-theme ttf-ms-fonts
+paru -S --needed --noconfirm p7zip entr zoxide eza ripgrep bat bat-extras fd fzf lazygit zathura zathura-pdf-mupdf htop seahorse grub-customizer ffmpeg font-manager mpv vlc reflector tty-clock xarchiver thunar-archive-plugin thunar-media-tags-plugin thunar-volman ttf-hack-nerd ttf-iosevka-nerd ttf-firacode-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd ttf-font-awesome otf-codenewroman-nerd plymouth plymouth-theme-arch10 dracula-gtk-theme nordic-theme nordzy-cursors nordzy-icon-theme whitesur-icon-theme ttf-ms-fonts
 
 echo "---------------------------------------------------------------"
-info "Instalando #-NVM (node version manager)"
+echo "Instalando #-NVM (node version manager)"
 export NVM_DIR="$HOME/.nvm"
 git clone https://github.com/nvm-sh/nvm.git $NVM_DIR
 cd $NVM_DIR
