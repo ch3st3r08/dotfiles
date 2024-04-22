fe764ceb116b9e5e5c9768d0427f713c571a6b20
Chester <nestor_chavez@hotmail.es>
2024-04-21 23:09:07 -0600

    Modifying installation file, new aliases

--- bin/bin/installSystemArch.sh
+++ bin/bin/installSystemArch.sh
@@ -1,21 +1,60 @@
 #!/bin/bash
 
+function Help() {
+	echo "Install the system and configurations."
+	echo
+	echo "Syntax: $0 [-b|m|w|h|d]"
+	echo "options:"
+	echo "-b  Enable debug messages"
+	echo "-m  Virtual Machine installation"
+	echo "-w  Desktop name (hypr | xfce4) default 'xfce4'"
+	echo "-d  Print installation arguments"
+	echo "-h  Print this message."
+	echo
+}
+
+function Dry() {
+	echo "----------------------------"
+	echo "Valor de Debug = $_DEBUG"
+	echo "Valor de IS_VM = $IS_VM"
+	echo "Valor de WM = $WM"
+	echo "----------------------------"
+}
+
+#Default variables
 _DEBUG=0
-IS_VM=1
-WM="hypr" #xfce4
+IS_VM=0
+WM="xfce4" #hypr
 
-pre_packages=""
-if [ -z $(which stow 2>/dev/null) ]; then
-	pre_packages="${pre_packages} stow"
-fi
+while getopts ":hbmdw:" option; do
+	case $option in
+	h)
+		Help
+		exit
+		;;
+	b)
+		_DEBUG=1
+		;;
+	m)
+		IS_VM=1
+		;;
+	w)
+		WM=$OPTARG
+		;;
+	d)
+		Dry
+		exit
+		;;
+	\?)
+		echo "Error: Invalid option"
+		Help
+		exit
+		;;
+	esac
+done
 
-if [ -z $(which xdg-user-dirs-update 2>/dev/null) ]; then
-	pre_packages="${pre_packages} xdg-user-dirs"
-fi
-
-if ! [ -z "$pre_packages" ]; then
-	sudo pacman -S --needed --noconfirm $pre_packages
-fi
+# Core packages for installation
+sudo pacman -S --needed --noconfirm git stow xdg-user-dirs-update
 
 cd $HOME
 
@@ -23,13 +62,16 @@ cd $HOME
 xdg-user-dirs-update
 
 #Descargar e instalar Paru (AUR helper)
-git clone https://aur.archlinux.org/paru-bin.git Sources/paru
-cd Sources/paru
-makepkg -si
+if [ -z $(which paru 2>/dev/null) ]; then
+	git clone https://aur.archlinux.org/paru-bin.git Sources/paru
+	cd $HOME/Sources/paru
+	makepkg -si --noconfirm
+	cd $HOME
+fi
 
-cd $HOME
-
-mv $HOME/.bashrc $HOME/.bashrc.bak
+if [ ! -f $HOME/.bashrc.bak ]; then
+	mv .bashrc .bashrc.bak
+fi
 
 cd .dotfiles
 stow -S alacritty pdf-reader nvim starship git tmux zsh sources music bin bash misc
@@ -39,10 +81,10 @@ echo "Instalando archivos necesarios"
 paru -S --needed --noconfirm curl pacman-contrib networkmanager sddm network-manager-applet zsh tmux gvfs gvfs-smb gvfs-nfs pipewire pipewire-audio pipewire-pulse wireplumber gst-plugin-pipewire thunar xdg-utils fastfetch neovim starship alacritty unzip pavucontrol brave-browser gtk-engine-murrine mpd ncmpcpp rhythmbox iwd libva-mesa-driver intel-media-driver sound-theme-freedesktop xf86-video-amdgpu
 
 if [ $WM == "hypr" ]; then
-        stow -S hypr waybar rofi
-	paru -S --needed --noconfirm hyprland xdg-desktop-portal-hyprland waybar rofi-lbonn-wayland swaync hypridle hyprlock hyprcursor swaybg nwg-bar nwg-look-bin waypaper polkit polkit-gnome desktop-file-utils nwg-displays nwg-drawer-bin nwg-icon-picker nwg-menu-bin hyprpicker wf-recorder ristretto cliphist qt5-wayland qt6-wayland swappy slurp grim
+	stow -S hypr waybar rofi
+	paru -S --needed --noconfirm hyprland xdg-desktop-portal-hyprland waybar rofi-lbonn-wayland swaync hypridle hyprlock hyprcursor swaybg nwg-bar-bin nwg-look-bin waypaper polkit polkit-gnome desktop-file-utils nwg-displays nwg-drawer-bin nwg-icon-picker nwg-menu-bin hyprpicker wf-recorder ristretto cliphist qt5-wayland qt6-wayland swappy slurp grim
 else
-        stow -S xfce4
+	stow -S xfce4
 	paru -S --needed --noconfirm xorg-server xorg-xinit xfce4 xfce4-goodies mugshot wmctrl timeshift-gtk xclip menulibre
 fi
 
