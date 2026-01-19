f5afaa04bd7a11da3a1290f9c83e94054b86e1fe
Chester <nestor_chavez@hotmail.es>
2026-01-18 19:36:30 -0600

    ADD: new ansible roles, dotfiles script

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 echo "Installing necessary packages"
 
-#sudo pacman -S --noconfirm --needed ansible-core xdg-user-dirs
+#sudo pacman -S --noconfirm --needed ansible-core xdg-user-dirs python-pexpect sshpass
 
 echo "Creating xdg directories"
 #xdg-user-dirs-update
 
 echo "Installing system"
-ansible-playbook ansible/main.yml --ask-become-pass -e "do_pass=$1" "$2"
+ansible-playbook ansible/main.yml --ask-become-pass -e "do_pass=$1" "de=$2"
 
