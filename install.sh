470c7172a93a5afb6bf369b647afbbe4e2c524fd
Chester <nestor_chavez@hotmail.es>
2026-01-21 00:57:16 -0600

    ADD: copy buffer plugin for zsh, some refactoring

--- install.sh
+++ install.sh
@@ -1,15 +1,79 @@
 #!/usr/bin/env bash
+my_help ()
+{
+    echo "Usage: install.sh [-e DE name] [-t tags] [-l list DE] [-v] "
+    exit 0
+}
+my_error ()
+{
+  echo "Needs to specify a DE name or a tag. See help -h"
+  exit 1
+}
+
+while getopts "ce:lt:vh" opt; do
+  case $opt in
+    c)
+      CHECK="--check"
+      ;;
+    e)
+      DE="$OPTARG"  # Option argument is in $OPTARG
+      ;;
+    l)
+      LIST_DE=1
+      ;;
+    n)
+      TAG="$OPTARG"
+      ;;
+    v)
+      VERBOSE=1
+      ;;
+    h)
+      my_help
+      ;;
+    :)
+      echo "Error: -$OPTARG requires an argument"
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+
+if [[ -n "$TAG" ]]; then
+  TAG_LINE="--tags $TAG"
+fi
+if [[ -n "$DE" ]]; then
+  TAG_LINE="--tags main,$DE"
+fi
+if [[ -n "$LIST_DE" ]]; then
+  _list=$(find . -type d -name "*_desktop" -exec basename {} \; \
+    | sed 's/_desktop//')
+  echo -e "Available DE: \n$_list"
+  exit 0
+fi
+
+if [[ -z "$TAG_LINE" ]]; then
+  my_error
+fi
 
 echo "Installing necessary packages"
+sudo pacman -S --noconfirm --needed ansible-core xdg-user-dirs
 
-#sudo pacman -S --noconfirm --needed ansible-core xdg-user-dirs python-pexpect sshpass
+#Should we download bw here?
+# or should that be a different process?
+# maybe an ansible role?
+#
 
 echo "Creating xdg directories"
-#xdg-user-dirs-update
+xdg-user-dirs-update
 
 echo "Installing collections"
 ansible-galaxy install -r ansible/requirements.yml
 
 echo "Installing system"
-ansible-playbook ansible/main.yml --ask-become-pass -e "do_pass=$1" "de=$2"
-
+ansible-playbook ansible/main.yml --ask-become-pass $TAG_LINE $CHECK
